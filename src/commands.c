998da7718a161b69696c8df4a45ff7458ff7a7d0
jmingrove21 <min_k21@naver.com>
2017-10-09 02:41:58 +0900

    #2 prototype

--- src/commands.c
+++ src/commands.c
@@ -1,12 +1,14 @@
 #include <string.h>
-
+#include <stdio.h>
 #include "commands.h"
+#include <unistd.h>
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
-    return -1;
+   return -1;
 
   // TODO: Fill it!
+  chdir(argv[1]);
   return 0;
 }
 
@@ -14,17 +16,25 @@ int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  char buf[255];
+  getcwd(buf,255);
+  printf("%s\n",buf);
 
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  if(argc==2&&!strcmp(argv[0],"cd"))
+   return 1;
+  else
+   return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+  if(argc==1&&!strcmp(argv[0],"pwd"))
+   return 1;
+  else
+   return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,37 @@
 #include "utils.h"
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
+ *argc=0;
+ char* command_tmp=(char*)malloc(sizeof(char)*100);
+
+ strcpy(command_tmp,command);
+
+ for(int i=0;command_tmp[i]!='\0';i++){
+  if(command_tmp[i]=='\n')
+   command_tmp[i]==NULL;
+ }
+
+ char* token;
+ token=strtok(command_tmp," \t\n");
+
+ *argv=(char**)malloc(sizeof(char*)*100);
+
+ if(token==NULL){
+  (*argc)++;
+  *(*argv)=(char*)malloc(sizeof(char)*100);
+  strcpy(*(*argv),"");
+ }
+
+ while(token!=NULL){
+  *(*argv+(*argc))=(char*)malloc(sizeof(char)*100);
+  strcpy(*(*argv+(*argc)),token);
+  token=strtok(NULL," \t\n");
+  (*argc)++;
+ }
 }
+
