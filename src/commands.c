61728fef96262cf74ed89a56d9dd0bb5cfd933cf
jmingrove21 <min_k21@naver.com>
2017-10-09 16:17:37 +0900

    #3 modify commands.c

--- src/commands.c
+++ src/commands.c
@@ -8,8 +8,15 @@ int do_cd(int argc, char** argv) {
    return -1;
 
   // TODO: Fill it!
-  chdir(argv[1]);
+  int tmp=chdir(argv[1]);
+  //int chdir(const char* path) : change current directory
+  if(tmp==-1){
+   fprintf(stderr,"bash: cd: %s: No such file or directory\n",argv[1]);
   return 0;
+  }
+  else if(tmp==0){
+   return 0;
+  }
 }
 
 int do_pwd(int argc, char** argv) {
@@ -18,6 +25,7 @@ int do_pwd(int argc, char** argv) {
 
   char buf[255];
   getcwd(buf,255);
+  //char *getcwd(char *buf, size_t size) : get current directory
   printf("%s\n",buf);
 
   return 0;
@@ -27,8 +35,13 @@ int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
   if(argc==2&&!strcmp(argv[0],"cd"))
    return 1;
-  else
+  else if(argc==1&&!strcmp(argv[0],"cd")){
+  // If you only type 'cd', this command is not supported by this program!
    return 0;
+  }
+  else
+   // argc>2 -> Entered too much!
+   return 0;  
 }
 
 int validate_pwd_argv(int argc, char** argv) {
@@ -36,5 +49,6 @@ int validate_pwd_argv(int argc, char** argv) {
   if(argc==1&&!strcmp(argv[0],"pwd"))
    return 1;
   else
+  // This program is not supported pwd + any command!
    return 0;
 }
